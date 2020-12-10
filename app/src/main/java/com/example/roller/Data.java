d89cdb540e84093e79248f2819ade8c0028f21c6
dnpshuser <sk9632070@gmail.com>
2020-12-10 16:13:06 +0530

    Working fine but some problem in path

--- app/src/main/java/com/example/roller/Data.java
+++ app/src/main/java/com/example/roller/Data.java
@@ -19,35 +19,47 @@ public class Data {
 
         houses.add(new House(1, new LocatedAt(24.75, 84.41666666666667), "Aurangabad", "Bihar"));
         houses.add(new House(2, new LocatedAt(25.666666666666668, 85.2), "Bankipore", "Bihar"));
-        houses.add(new House(3, new LocatedAt(23.166666666666668, 84.21666666666667), "Barwa", "Bihar"));
-        houses.add(new House(4, new LocatedAt(26.8, 84.55), "Bettiah", "Bihar"));
-        houses.add(new House(5, new LocatedAt(25.25, 87.01666666666667), "Bhagalpur", "Bihar"));
-        houses.add(new House(6, new LocatedAt(24.683333333333334, 85.03333333333333), "Buddh Gaya", "Bihar"));
-        houses.add(new House(7, new LocatedAt(25.566666666666666, 84.01666666666667), "Buxar", "Bihar"));
-        houses.add(new House(8, new LocatedAt(25.783333333333335, 84.78333333333333), "Chhapra", "Bihar"));
-        houses.add(new House(9, new LocatedAt(25.266666666666666, 87.28333333333333), "Colgong", "Bihar"));
-        houses.add(new House(10, new LocatedAt(26.166666666666668, 85.95), "Darbhanga (Laheriasarai)", "Bihar"));
-        houses.add(new House(11, new LocatedAt(25.233333333333334, 84.28333333333333), "Dhankkgain", "Bihar"));
-        houses.add(new House(12, new LocatedAt(25.633333333333333, 85.08333333333333), "Dinapore", "Bihar"));
-        houses.add(new House(13, new LocatedAt(25.55, 84.2), "Dumraon", "Bihar"));
-        houses.add(new House(14, new LocatedAt(26.5, 84.5), "Gandak R.", "Bihar"));
-        houses.add(new House(15, new LocatedAt(42.81666666666667, 85.01666666666667), "Gaya", "Bihar"));
-        houses.add(new House(16, new LocatedAt(25.466666666666665, 86.63333333333334), "Gogri", "Bihar"));
-        houses.add(new House(17, new LocatedAt(26.466666666666665, 84.43333333333334), "Gopalganj", "Bihar"));
-        houses.add(new House(18, new LocatedAt(25.683333333333334, 85.95), "Hajipur", "Bihar"));
-        houses.add(new House(19, new LocatedAt(25.316666666666666, 85.53333333333333), "Jamalpur", "Bihar"));
-        houses.add(new House(20, new LocatedAt(26.583333333333332, 86.15), "Jyanagar", "Bihar"));
-        houses.add(new House(21, new LocatedAt(26.516666666666666, 85.35), "Kantai", "Bihar"));
-        houses.add(new House(22, new LocatedAt(25.5, 87.66666666666667), "Kathihar", "Bihar"));
-        houses.add(new House(23, new LocatedAt(26.166666666666668, 87.03333333333333), "Kishanganj", "Bihar"));
-        houses.add(new House(24, new LocatedAt(23.666666666666668, 84.55), "Mankheri", "Bihar"));
+        houses.add(new House(31, new LocatedAt(25.616666666666667, 85.21666666666667), "Patna", "Bihar"));
+
+        houses.add(new House(41, new LocatedAt(28.7041, 77.1025), "Delhi", "Delhi"));
+        houses.add(new House(51, new LocatedAt(19.0760, 72.8777), "mumbai", "Maharashtra"));
+        houses.add(new House(61, new LocatedAt(22.5726, 88.3639), "Kolkata", "West Bengal"));
+        houses.add(new House(71, new LocatedAt(12.9716, 77.5946), "Bengaluru", "Karnataka"));
+
+
+
+//        houses.add(new House(3, new LocatedAt(23.166666666666668, 84.21666666666667), "Barwa", "Bihar"));
+//        houses.add(new House(4, new LocatedAt(26.8, 84.55), "Bettiah", "Bihar"));
+//        houses.add(new House(5, new LocatedAt(25.25, 87.01666666666667), "Bhagalpur", "Bihar"));
+//        houses.add(new House(6, new LocatedAt(24.683333333333334, 85.03333333333333), "Buddh Gaya", "Bihar"));
+//        houses.add(new House(7, new LocatedAt(25.566666666666666, 84.01666666666667), "Buxar", "Bihar"));
+//        houses.add(new House(8, new LocatedAt(25.783333333333335, 84.78333333333333), "Chhapra", "Bihar"));
+//        houses.add(new House(9, new LocatedAt(25.266666666666666, 87.28333333333333), "Colgong", "Bihar"));
+//        houses.add(new House(10, new LocatedAt(26.166666666666668, 85.95), "Darbhanga (Laheriasarai)", "Bihar"));
+//        houses.add(new House(11, new LocatedAt(25.233333333333334, 84.28333333333333), "Dhankkgain", "Bihar"));
+//        houses.add(new House(12, new LocatedAt(25.633333333333333, 85.08333333333333), "Dinapore", "Bihar"));
+//        houses.add(new House(13, new LocatedAt(25.55, 84.2), "Dumraon", "Bihar"));
+//        houses.add(new House(14, new LocatedAt(26.5, 84.5), "Gandak R.", "Bihar"));
+//        houses.add(new House(15, new LocatedAt(42.81666666666667, 85.01666666666667), "Gaya", "Bihar"));
+//        houses.add(new House(16, new LocatedAt(25.466666666666665, 86.63333333333334), "Gogri", "Bihar"));
+//        houses.add(new House(17, new LocatedAt(26.466666666666665, 84.43333333333334), "Gopalganj", "Bihar"));
+//        houses.add(new House(18, new LocatedAt(25.683333333333334, 85.95), "Hajipur", "Bihar"));
+//        houses.add(new House(19, new LocatedAt(25.316666666666666, 85.53333333333333), "Jamalpur", "Bihar"));
+//        houses.add(new House(20, new LocatedAt(26.583333333333332, 86.15), "Jyanagar", "Bihar"));
+//        houses.add(new House(21, new LocatedAt(26.516666666666666, 85.35), "Kantai", "Bihar"));
+//        houses.add(new House(22, new LocatedAt(25.5, 87.66666666666667), "Kathihar", "Bihar"));
+//        houses.add(new House(23, new LocatedAt(26.166666666666668, 87.03333333333333), "Kishanganj", "Bihar"));
+//        houses.add(new House(24, new LocatedAt(23.666666666666668, 84.55), "Mankheri", "Bihar"));
+
+
+
+
 //        houses.add(new House(25, new LocatedAt(25.4, 85.91666666666667), "Mokameh", "Bihar"));
 //        houses.add(new House(26, new LocatedAt(26.666666666666668, 85.23333333333333), "Motihari", "Bihar"));
 //        houses.add(new House(27, new LocatedAt(25.383333333333333, 86.5), "Munger", "Bihar"));
 //        houses.add(new House(28, new LocatedAt(26.116666666666667, 85.45), "Muzaffarpur", "Bihar"));
 //        houses.add(new House(29, new LocatedAt(24.883333333333333, 85.58333333333333), "Nawada", "Bihar"));
 //        houses.add(new House(30, new LocatedAt(24.5, 87.33333333333333), "Naya Dumka", "Bihar"));
-//        houses.add(new House(31, new LocatedAt(25.616666666666667, 85.21666666666667), "Patna", "Bihar"));
 //        houses.add(new House(32, new LocatedAt(25.816666666666666, 87.51666666666667), "Purnia", "Bihar"));
 //        houses.add(new House(33, new LocatedAt(22.266666666666666, 85.25), "Safanda", "Bihar"));
 //        houses.add(new House(34, new LocatedAt(26.783333333333335, 84.8), "Sagaull", "Bihar"));
@@ -75,13 +87,18 @@ public class Data {
             assert h != null;
             assert h1 != null;
             Node newNode = new Node(h1.getId(),Util.findDistance(h.getLocation(),h1.getLocation()));
+            Node newNode1 = new Node(h.getId(),Util.findDistance(h.getLocation(),h1.getLocation()));
             List<Node> n = adj.get(h.getId());
+            List<Node> m = adj.get(h.getId());
             if(n==null){
                 n = new ArrayList<>();
             }
+            if(m==null)
+                m = new ArrayList<>();
             n.add(newNode);
-
+            m.add(newNode1);
             adj.put(h.getId(),n);
+            adj.put(h1.getId(),m);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -90,5 +107,26 @@ public class Data {
     void connecting(){
         directory = getIdByCity();
         connectCities("arrah","patna");
+        connectCities("aurangabad","patna");
+        connectCities("bankipore","patna");
+
+
+
+        connectCities("delhi","patna");
+        connectCities("mumbai","patna");
+        connectCities("kolkata","patna");
+        connectCities("bengaluru","patna");
+
+        connectCities("mumbai","delhi");
+        connectCities("kolkata","delhi");
+        connectCities("bengaluru","delhi");
+
+        connectCities("kolkata","mumbai");
+        connectCities("bengaluru","mumbai");
+
+        connectCities("bengaluru","kolkata");
+
+
+
     }
 }
