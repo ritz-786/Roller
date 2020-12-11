346a98af434f30b104805b5625c4eb74dfcb249d
ritz-786 <ritikraj7255@gmail.com>
2020-12-11 22:37:15 +0530

    added location

--- app/src/main/java/com/example/roller/User.java
+++ app/src/main/java/com/example/roller/User.java
@@ -2,15 +2,14 @@ package com.example.roller;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import java.util.HashMap;
 
@@ -22,17 +21,14 @@ import java.util.HashMap;
 public class User extends Fragment {
 
 
-
-
     // Instantiate Interface User_info
     UserInfo userInfo;
 
-    public void getContext(Activity activity)
-    {
+    public void getContext(Activity activity) {
         try {
-            userInfo = (UserInfo)activity;
-        }catch (ClassCastException e){
-            Toast.makeText(activity, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            userInfo = (UserInfo) activity;
+        } catch (ClassCastException e) {
+            Toast.makeText(activity, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
 
@@ -68,45 +64,43 @@ public class User extends Fragment {
     }
 
     Button Ordered;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_user, container, false);
 
-        EditText shoe =  view.findViewById(R.id.Shoes);
+        EditText shoe = view.findViewById(R.id.Shoes);
         EditText watch = view.findViewById(R.id.Watch);
         EditText shirt = view.findViewById(R.id.Shirts);
         EditText belts = view.findViewById(R.id.Belts);
-        EditText  trouser = view.findViewById(R.id.Trousers);
+        EditText trouser = view.findViewById(R.id.Trousers);
         EditText socks = view.findViewById(R.id.Socks);
         EditText laptops = view.findViewById(R.id.Laptops);
 
 
         Ordered = view.findViewById(R.id.Order);
 
-        Ordered.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        Ordered.setOnClickListener(view1 -> {
 
-                HashMap<String, Integer> hashMap = new HashMap<>();
+            HashMap<String, Integer> hashMap = new HashMap<>();
 
 
-                hashMap.put("Shoe",Integer.parseInt(shoe.getText().toString()));
-                hashMap.put("Watch", Integer.parseInt(watch.getText().toString()));
-                hashMap.put("Shirt", Integer.parseInt(shirt.getText().toString()));
-                hashMap.put("Belts", Integer.parseInt(belts.getText().toString()));
-                hashMap.put("Trousers", Integer.parseInt(trouser.getText().toString()));
-                hashMap.put("Socks", Integer.parseInt(socks.getText().toString()));
-                hashMap.put("Laptops", Integer.parseInt(laptops.getText().toString()));
-                userInfo.Info_User(hashMap);
-            }
+            hashMap.put("Shoe", Integer.parseInt(shoe.getText().toString().equals("") ? "0" : shoe.getText().toString()));
+            hashMap.put("Watch", Integer.parseInt(watch.getText().toString().equals("") ? "0" : watch.getText().toString()));
+            hashMap.put("Shirt", Integer.parseInt(shirt.getText().toString().equals("") ? "0" : shirt.getText().toString()));
+            hashMap.put("Belts", Integer.parseInt(belts.getText().toString().equals("") ? "0" : belts.getText().toString()));
+            hashMap.put("Trousers", Integer.parseInt(trouser.getText().toString().equals("") ? "0" : trouser.getText().toString()));
+            hashMap.put("Socks", Integer.parseInt(socks.getText().toString().equals("") ? "0" : socks.getText().toString()));
+            hashMap.put("Laptops", Integer.parseInt(laptops.getText().toString().equals("") ? "0" : laptops.getText().toString()));
+            userInfo.Info_User(hashMap);
         });
 
         return view;
     }
 
-    public interface UserInfo{
+    public interface UserInfo {
 
         public void Info_User(HashMap<String, Integer> hashMap);
     }
